cee5f63967fd08c0a91499dbceb6f03b48ae3c50
Никита Лукьяница <59867180+thnik911@users.noreply.github.com>
2022-04-12 19:17:17 +0500

    Update auth.php
    Визуальные изменения кода для удобства чтения

--- auth.php
+++ auth.php
@@ -1,9 +1,7 @@
 <?
 
-
 $queryUrl = 'https://test.bitrix24.ru/rest/3732/m7k******hvantg/crm.lead.add.json';
 
-$domain            = 'test.bitrix24.ru'; //адрес портала Битиркс24 
-$auth              = 'm7k******hvantg'; //секретный код
-$user              = '3732'; // id пользователя администратора
-?>
+$domain = 'test.bitrix24.ru'; //адрес портала Битиркс24
+$auth = 'm7k******hvantg'; //секретный код
+$user = '3732'; // id пользователя администратора
